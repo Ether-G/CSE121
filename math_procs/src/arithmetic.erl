60829f40ea574cb84c2abcf95705a1b0b05ac6ec
Ether-G <toastedben@gmail.com>
2025-02-01 23:47:09 -0600

    math procs updated

--- math_procs/src/arithmetic.erl
+++ math_procs/src/arithmetic.erl
@@ -1,12 +1,121 @@
 -module(arithmetic).
--export([start_factorializer/0,start_adder/0,start_subtracter/0,start_multiplier/0,start_divider/0,
-		 factorializer/0,adder/0,subtracter/0,multiplier/0,divider/0,
-		 factorial_of/2,add/3,subtract/3,multiply/3,divide/3]).
+-export([start_factorializer/0, start_adder/0, start_subtracter/0, start_multiplier/0, start_divider/0,
+         factorializer/0, adder/0, subtracter/0, multiplier/0, divider/0,
+         factorial_of/2, add/3, subtract/3, multiply/3, divide/3]).
 
-%%
-%% Put your functions, described in the task HTML file here.
-%%
+%% Client Interface Functions
 
+factorial_of(Server, N) when is_integer(N), N >= 0 ->
+    Server ! {self(), factorial, N},
+    receive
+        {Server, Result} -> Result
+    end;
+factorial_of(Server, N) when is_integer(N) ->
+    {fail, N, is_negative};
+factorial_of(Server, N) when is_float(N) ->
+    {fail, N, is_not_integer};
+factorial_of(Server, N) ->
+    {fail, N, is_not_integer}.
+
+add(Server, X, Y) when is_number(X), is_number(Y) ->
+    Server ! {self(), add, X, Y},
+    receive
+        {Server, Result} -> Result
+    end;
+add(Server, X, _Y) when not is_number(X) ->
+    {fail, X, is_not_number};
+add(Server, _X, Y) ->
+    {fail, Y, is_not_number}.
+
+subtract(Server, X, Y) when is_number(X), is_number(Y) ->
+    Server ! {self(), subtract, X, Y},
+    receive
+        {Server, Result} -> Result
+    end;
+subtract(Server, X, _Y) when not is_number(X) ->
+    {fail, X, is_not_number};
+subtract(Server, _X, Y) ->
+    {fail, Y, is_not_number}.
+
+multiply(Server, X, Y) when is_number(X), is_number(Y) ->
+    Server ! {self(), multiply, X, Y},
+    receive
+        {Server, Result} -> Result
+    end;
+multiply(Server, X, _Y) when not is_number(X) ->
+    {fail, X, is_not_number};
+multiply(Server, _X, Y) ->
+    {fail, Y, is_not_number}.
+
+divide(Server, X, Y) when is_number(X), is_number(Y) ->
+    Server ! {self(), divide, X, Y},
+    receive
+        {Server, Result} -> Result
+    end;
+divide(Server, X, _Y) when not is_number(X) ->
+    {fail, X, is_not_number};
+divide(Server, _X, Y) ->
+    {fail, Y, is_not_number}.
+
+%% Server Process Functions
+
+factorializer() ->
+    receive
+        {From, factorial, N} ->
+            Result = calc_factorial(N),
+            From ! {self(), Result},
+            factorializer()
+    end.
+
+adder() ->
+    receive
+        {From, add, X, Y} ->
+            From ! {self(), X + Y},
+            adder()
+    end.
+
+subtracter() ->
+    receive
+        {From, subtract, X, Y} ->
+            From ! {self(), X - Y},
+            subtracter()
+    end.
+
+multiplier() ->
+    receive
+        {From, multiply, X, Y} ->
+            From ! {self(), X * Y},
+            multiplier()
+    end.
+
+divider() ->
+    receive
+        {From, divide, X, Y} ->
+            From ! {self(), X / Y},
+            divider()
+    end.
+
+%% Start Functions
+
+start_factorializer() ->
+    spawn(?MODULE, factorializer, []).
+
+start_adder() ->
+    spawn(?MODULE, adder, []).
+
+start_subtracter() ->
+    spawn(?MODULE, subtracter, []).
+
+start_multiplier() ->
+    spawn(?MODULE, multiplier, []).
+
+start_divider() ->
+    spawn(?MODULE, divider, []).
+
+%% Helper Functions
+
+calc_factorial(0) -> 1;
+calc_factorial(N) -> N * calc_factorial(N-1).
 
 -ifdef(EUNIT).
 %%
@@ -18,107 +127,107 @@
 
 factorializer_test_() ->
 {setup, 
-	fun() -> % runs before any of the tests
-			Pid = spawn(?MODULE, factorializer, []), 	
-			register(test_factorializer, Pid)
-		end, 
-	% fun(_)-> % runs after all of the tests
-		% there is no teardown needed, so this fun doesn't need to be implemented.
-	% end, 
-	% factorializer tests start here
-	[ ?_assertEqual(120,  factorial_of(test_factorializer,  5)),  % happy path, tests the obvious case.
-	  % test less obvious or edge cases
-	  ?_assertEqual(1, factorial_of(test_factorializer, 0)), 
-	  ?_assertEqual({fail, -3, is_negative}, factorial_of(test_factorializer, -3)), 
-	  ?_assertEqual({fail, bob, is_not_integer}, factorial_of(test_factorializer, bob)), 
-	  ?_assertEqual({fail, 5.0, is_not_integer}, factorial_of(test_factorializer, 5.0))
-	]
+    fun() -> % runs before any of the tests
+            Pid = spawn(?MODULE, factorializer, []),     
+            register(test_factorializer, Pid)
+        end, 
+    % fun(_)-> % runs after all of the tests
+        % there is no teardown needed, so this fun doesn't need to be implemented.
+    % end, 
+    % factorializer tests start here
+    [ ?_assertEqual(120,  factorial_of(test_factorializer,  5)),  % happy path, tests the obvious case.
+      % test less obvious or edge cases
+      ?_assertEqual(1, factorial_of(test_factorializer, 0)), 
+      ?_assertEqual({fail, -3, is_negative}, factorial_of(test_factorializer, -3)), 
+      ?_assertEqual({fail, bob, is_not_integer}, factorial_of(test_factorializer, bob)), 
+      ?_assertEqual({fail, 5.0, is_not_integer}, factorial_of(test_factorializer, 5.0))
+    ]
 }.
 
 adder_test_() ->
 {setup, 
-	fun()->%runs before any of the tests
-			Pid = spawn(?MODULE, adder, []), 	
-			register(test_adder, Pid)
-		end, 
-	%fun(_)->%runs after all of the tests
-		%there is no teardown needed, so this fun doesn't need to be implemented.
-	%end, 
-	%factorializer tests start here
-	[ ?_assertEqual(8, add(test_adder, 5, 3)), %happy path
-	  % test less obvious or edge cases
-	  ?_assertEqual(0, add(test_adder, 0, 0)), 
-	  ?_assertEqual(0.0, add(test_adder, 0.0, 0.0)), 
-	  ?_assertEqual(0, add(test_adder, -5, 5)), 
-	  ?_assertEqual(1.5, add(test_adder, 0.75, 0.75)), 
-	  ?_assertEqual({fail, bob, is_not_number}, add(test_adder, bob, 3)), 
-	  ?_assertEqual({fail, sue, is_not_number}, add(test_adder, 3, sue)), 
-	  ?_assertEqual({fail, bob, is_not_number}, add(test_adder, bob, sue))
-	]
+    fun()->%runs before any of the tests
+            Pid = spawn(?MODULE, adder, []),     
+            register(test_adder, Pid)
+        end, 
+    %fun(_)->%runs after all of the tests
+        %there is no teardown needed, so this fun doesn't need to be implemented.
+    %end, 
+    %factorializer tests start here
+    [ ?_assertEqual(8, add(test_adder, 5, 3)), %happy path
+      % test less obvious or edge cases
+      ?_assertEqual(0, add(test_adder, 0, 0)), 
+      ?_assertEqual(0.0, add(test_adder, 0.0, 0.0)), 
+      ?_assertEqual(0, add(test_adder, -5, 5)), 
+      ?_assertEqual(1.5, add(test_adder, 0.75, 0.75)), 
+      ?_assertEqual({fail, bob, is_not_number}, add(test_adder, bob, 3)), 
+      ?_assertEqual({fail, sue, is_not_number}, add(test_adder, 3, sue)), 
+      ?_assertEqual({fail, bob, is_not_number}, add(test_adder, bob, sue))
+    ]
 }.
 
 subtracter_test_() ->
 {setup, 
-	fun()->%runs before any of the tests
-			Pid = spawn(?MODULE, subtracter, []), 	
-			register(test_subtracter, Pid)
-		end, 
-	%fun(_)->%runs after all of the tests
-		%there is no teardown needed, so this fun doesn't need to be implemented.
-	%end, 
-	%factorializer tests start here
-	[ ?_assertEqual(2, subtract(test_subtracter, 5, 3)), %happy path
-	  % test less obvious or edge cases
-	  ?_assertEqual(0, subtract(test_subtracter, 0, 0)), 
-	  ?_assertEqual(0.0, subtract(test_subtracter, 0.0, 0.0)), 
-	  ?_assertEqual(-10, subtract(test_subtracter, -5, 5)), 
-	  ?_assertEqual(0.75, subtract(test_subtracter, 1.5, 0.75)), 
-	  ?_assertEqual({fail, bob, is_not_number}, subtract(test_subtracter, bob, 3)), 
-	  ?_assertEqual({fail, sue, is_not_number}, subtract(test_subtracter, 3, sue)), 
-	  ?_assertEqual({fail, bob, is_not_number}, subtract(test_subtracter, bob, sue))
-	]
+    fun()->%runs before any of the tests
+            Pid = spawn(?MODULE, subtracter, []),     
+            register(test_subtracter, Pid)
+        end, 
+    %fun(_)->%runs after all of the tests
+        %there is no teardown needed, so this fun doesn't need to be implemented.
+    %end, 
+    %factorializer tests start here
+    [ ?_assertEqual(2, subtract(test_subtracter, 5, 3)), %happy path
+      % test less obvious or edge cases
+      ?_assertEqual(0, subtract(test_subtracter, 0, 0)), 
+      ?_assertEqual(0.0, subtract(test_subtracter, 0.0, 0.0)), 
+      ?_assertEqual(-10, subtract(test_subtracter, -5, 5)), 
+      ?_assertEqual(0.75, subtract(test_subtracter, 1.5, 0.75)), 
+      ?_assertEqual({fail, bob, is_not_number}, subtract(test_subtracter, bob, 3)), 
+      ?_assertEqual({fail, sue, is_not_number}, subtract(test_subtracter, 3, sue)), 
+      ?_assertEqual({fail, bob, is_not_number}, subtract(test_subtracter, bob, sue))
+    ]
 }.
 
 multiplier_test_() ->
 {setup, 
-	fun()->%runs before any of the tests
-			Pid = spawn(?MODULE, multiplier, []), 	
-			register(test_multiplier, Pid)
-		end, 
-	%fun(_)->%runs after all of the tests
-		%there is no teardown needed, so this fun doesn't need to be implemented.
-	%end, 
-	%factorializer tests start here
-	[ ?_assertEqual(15, multiply(test_multiplier, 5, 3)), %happy path
-	  % test less obvious or edge cases
-	  ?_assertEqual(0, multiply(test_multiplier, 0, 0)), 
-	  ?_assertEqual(0.0, multiply(test_multiplier, 0.0, 0.0)), 
-	  ?_assertEqual(-25, multiply(test_multiplier, -5, 5)), 
-	  ?_assertEqual(1.125, multiply(test_multiplier, 1.5, 0.75)), 
-	  ?_assertEqual({fail, bob, is_not_number}, multiply(test_multiplier, bob, 3)), 
-	  ?_assertEqual({fail, sue, is_not_number}, multiply(test_multiplier, 3, sue)), 
-	  ?_assertEqual({fail, bob, is_not_number}, multiply(test_multiplier, bob, sue))
-	]
+    fun()->%runs before any of the tests
+            Pid = spawn(?MODULE, multiplier, []),     
+            register(test_multiplier, Pid)
+        end, 
+    %fun(_)->%runs after all of the tests
+        %there is no teardown needed, so this fun doesn't need to be implemented.
+    %end, 
+    %factorializer tests start here
+    [ ?_assertEqual(15, multiply(test_multiplier, 5, 3)), %happy path
+      % test less obvious or edge cases
+      ?_assertEqual(0, multiply(test_multiplier, 0, 0)), 
+      ?_assertEqual(0.0, multiply(test_multiplier, 0.0, 0.0)), 
+      ?_assertEqual(-25, multiply(test_multiplier, -5, 5)), 
+      ?_assertEqual(1.125, multiply(test_multiplier, 1.5, 0.75)), 
+      ?_assertEqual({fail, bob, is_not_number}, multiply(test_multiplier, bob, 3)), 
+      ?_assertEqual({fail, sue, is_not_number}, multiply(test_multiplier, 3, sue)), 
+      ?_assertEqual({fail, bob, is_not_number}, multiply(test_multiplier, bob, sue))
+    ]
 }.
 
 divider_test_() ->
 {setup, 
-	fun()->%runs before any of the tests
-			Pid = spawn(?MODULE, divider, []), 	
-			register(test_divider, Pid)
-		end, 
-	%fun(_)->%runs after all of the tests
-		%there is no teardown needed, so this fun doesn't need to be implemented.
-	%end, 
-	%factorializer tests start here
-	[ ?_assert((1.6 < divide(test_divider, 5, 3)) and (divide(test_divider, 5, 3) < 1.7)), %happy path
-	  % test less obvious or edge cases
-	  ?_assertEqual(-1.0, divide(test_divider, -5, 5)), 
-	  ?_assertEqual(2.0, divide(test_divider, 1.5, 0.75)), 
-	  ?_assertEqual({fail, bob, is_not_number}, divide(test_divider, bob, 3)), 
-	  ?_assertEqual({fail, sue, is_not_number}, divide(test_divider, 3, sue)), 
-	  ?_assertEqual({fail, bob, is_not_number}, divide(test_divider, bob, sue))
-	]
+    fun()->%runs before any of the tests
+            Pid = spawn(?MODULE, divider, []),     
+            register(test_divider, Pid)
+        end, 
+    %fun(_)->%runs after all of the tests
+        %there is no teardown needed, so this fun doesn't need to be implemented.
+    %end, 
+    %factorializer tests start here
+    [ ?_assert((1.6 < divide(test_divider, 5, 3)) and (divide(test_divider, 5, 3) < 1.7)), %happy path
+      % test less obvious or edge cases
+      ?_assertEqual(-1.0, divide(test_divider, -5, 5)), 
+      ?_assertEqual(2.0, divide(test_divider, 1.5, 0.75)), 
+      ?_assertEqual({fail, bob, is_not_number}, divide(test_divider, bob, 3)), 
+      ?_assertEqual({fail, sue, is_not_number}, divide(test_divider, 3, sue)), 
+      ?_assertEqual({fail, bob, is_not_number}, divide(test_divider, bob, sue))
+    ]
 }.
 
 -endif.
